5583343d62e757ab7fd721cbdeb224547e09315f
Akash Agrawal <akash@ip-172-31-46-215.ap-south-1.compute.internal>
2020-09-29 11:53:24 +0000

    adding

--- .travis/update.sh
+++ .travis/update.sh
@@ -3,22 +3,22 @@ clone_git(){
   git config --global filter.lfs.smudge "git-lfs smudge --skip"
   timeout 60  git clone --progress --verbose https://${GH_TOKEN}@github.com/akash1808/django-deployment.git
   cd django-deployment
-  git fetch
-  git checkout master
+  timeout 10 git fetch
+  timeout 10 git checkout master
 }
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  timeout 10 git config --global user.email "travis@travis-ci.org"
+  timeout 10 git config --global user.name "Travis CI"
 }
 
 commit_files() {
   cat $1 > check.txt
-  git add check.txt
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  timeout 10 git add check.txt
+  timeout 10 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git push --quiet --set-upstream origin master 
+  timeout 60 git push --quiet --set-upstream origin master 
 }
 
 clone_git
