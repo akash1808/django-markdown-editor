62c1ced63263cd0dccad5f57d1a179eb60f7492f
Akash Agrawal <akash@ip-172-31-46-215.ap-south-1.compute.internal>
2020-09-29 15:36:05 +0000

    test

--- .travis/update.sh
+++ .travis/update.sh
@@ -3,7 +3,8 @@ git config --global filter.lfs.smudge "git-lfs smudge --skip"
 timeout 60  git clone --progress --verbose https://${GH_TOKEN}@github.com/akash1808/django-deployment.git
 cd django-deployment
 timeout 10 git fetch
-timeout 10 git checkout master
+timeout 10 git checkout masteri
+echo "complete"
 timeout 10 git config --global user.email "travis@travis-ci.org"
 timeout 10 git config --global user.name "Travis CI"
 
