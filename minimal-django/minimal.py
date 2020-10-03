145e589c7515fe2ac459dc4a5f8e0ddc9aafc59e
Akash Agrawal <akash@ip-172-31-46-215.ap-south-1.compute.internal>
2020-10-03 09:44:37 +0000

    demo check

--- minimal-django/minimal.py
+++ minimal-django/minimal.py
@@ -13,7 +13,7 @@ settings.configure(
 
 
 def index(request):
-    return HttpResponse('<h1>A minimal Django response!</h1>')
+    return HttpResponse('<h1>A minimal Django response!check123/h1>')
 
 urlpatterns = [
     url(r'^$', index),
